edbbe8bc1e67138f1d150e91b0a9162b085e89c1
keelanpattersoncaesar <keelanpattersoncaesar@gmail.com>
2022-05-30 16:50:25 -0700

    general changes for heroku test

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import FruitBasketIcon from "../assets/fruitbasketicon.png";
 
 const NavBar = () => {
   return (
-    <nav className="font-Roboto text-center bg-gradient-to-r from-orange-400 to-red-400 px-2 sm:px-4 py-2.5 rounded dark:bg-black-800">
-      <div className="container flex flex-wrap justify-between items-center ">
-        <a href="/" className="flex items-center">
-          <img
-            src="https://i.ibb.co/mXvc7Ym/basket-edited-edited-1.png"
-            alt="fruitbasket"
-            className="h-10 w-10"
-          />
-          <span className="self-center text-xl font-semibold whitespace-nowrap text-white">
-            FruityCounter
-          </span>
-        </a>
+    <nav className="font-Roboto text-center bg-gradient-to-r from-orange-400 to-red-400 px-2 sm:px-4 py-2.5 dark:bg-black-800">
+      <div className="flex space-between justify-between items-center md:flex flex-col">
+        <div className="hidden:md ">
+          <a href="/" className="flex items-center">
+            <img
+              src={FruitBasketIcon}
+              alt="fruitbasket"
+              className="h-10 w-10"
+            />
+            <span className="text-center text-xl font-extrabold whitespace-nowrap pl-2 text-white">
+              FruityCounter
+            </span>
+          </a>
+        </div>
 
         {/* MOBILE MENU */}
         <div className="w-full hidden:md block md:w-auto" id="mobile-menu">
-          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+          <ul className="flex flex-col mt-2 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
             <li>
               <Link
                 to="/"
-                className="block py-2 pr-2 pl-2 rounded md:bg-transparent md:p-0 text-white"
+                className="block my-1 rounded md:bg-transparent md:p-0 text-white"
                 aria-current="page"
               >
                 Home
@@ -31,7 +34,7 @@ const NavBar = () => {
             <li>
               <Link
                 to="/about"
-                className="block py-2 pr-2 pl-2 text-white rounded md:bg-transparent md:p-0"
+                className="block my-1 text-white rounded md:bg-transparent md:p-0"
                 aria-current="page"
               >
                 About
@@ -40,7 +43,7 @@ const NavBar = () => {
             <li>
               <Link
                 to="/contact"
-                className="block py-2 pr-2 pl-2 text-white rounded md:bg-transparent md:p-0"
+                className="block my-1 text-white rounded md:bg-transparent md:p-0"
                 aria-current="page"
               >
                 Contact
